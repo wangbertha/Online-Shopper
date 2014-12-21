3b40ce1805bd55784ebf0fcda276599fbd6f7eba
Alan Liu <AlanL.Liu@yahoo.com>
2014-12-20 22:58:39 -0500

    added a couple of routes to index

--- hello.py
+++ hello.py
@@ -1,24 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 import controllers
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', username = "")
 
-@app.route('/yaleims')
-def yaleims():
-	wks = controllers.overall()
-	return render_template('yaleims/home.html', wks=wks)
+@app.route('/', methods =['POST'])
+def login():
+	username = request.form['username']
 
-@app.route('/habitar')
-def habitar():
-	wks = controllers.overall()
-	return render_template('habitar/index.html', wks=wks)
 
+@app.route('/<username>')
+def userPage(username):
+	user = username
+	return render_template('index.html', username = user)
+
+
+@app.route('/logout')
+def logout():
+	return redirect('/')
+
+# test code 
 @app.route('/user/<username>')
 def hello(username):
 	return 'hey %s!' % username
 
 if __name__ == '__main__':
     app.run(debug=True)
+
